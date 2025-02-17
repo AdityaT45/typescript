7fe0e8c08078e9db483ff7070aa567a8986a718d
AdityaT45 <adityatodmal47@gmail.com>
2025-02-17 23:53:55 +0530

    basic information about ts datatypes

---
+++ index.ts
@@ -0,0 +1,112 @@
+// function add(a:number,v:number):number {
+//     return a+v;
+
+// }
+
+// let c=add(5,7)
+// console.log(c);
+
+
+let num:number=20;
+// console.log(num);
+
+let str:string='asd'
+// console.log(str);
+
+
+let isAvaliable:boolean=true;
+// console.log(isAvaliable);
+
+let namea:null=null;
+// console.log(namea);
+
+
+let numArr:(number|string)[]=[2,3,5,7,3,"as","sdas"]
+// console.log(numArr);
+
+
+
+let obj:{name:string,age:string,}={name:"aditya",age:"22"}
+// console.log(obj);
+
+obj={...obj}
+// console.log(obj);
+
+
+//any
+
+let value;
+
+// value="adoyua"
+// console.log(typeof value);
+// value=24
+// console.log(typeof value);
+// value=false
+// console.log(typeof value);
+
+
+//////////////////////////////////////////////////Array
+
+// let arr1:(number|boolean|string)[]=[1,2,3,4,5,false,"a"]
+// let arr2:(string)[]=["adityaa"]
+
+// // let arr3=arr1.concat(arr2)
+// let arr3=arr1.map(ele=>ele +" hii aditya")
+
+// // let arr3:(number|string|boolean)[]=[...arr1,...arr2]
+// console.log(arr3);
+
+
+
+//////////////////////////////////////////////// Tuple
+
+// let tuple:[number,string,number]=[12,"ad",12]
+
+// let result=tuple.map(ele=>ele + " this is tuple answer")
+// console.log(JSON.stringify(result));
+
+
+
+
+// let data: unknown;
+
+// data = "Hello";  // ✅ Allowed
+// data = 10;       // ✅ Allowed
+
+// if(typeof data==="number"){
+// console.log(data.toString()); 
+// }
+
+
+/////////////////////////////////////////interface
+
+interface Person{
+    name:string,
+    age:number,
+    greet():void
+}
+
+let person1:Person={
+    name:"aditya",
+    age:22,
+    greet() {
+        console.log("this is person 1")
+    }
+}
+
+let person2:Person={
+    name:"ganesh",
+    age:32,
+    greet() {
+        console.log("this is ganesh")
+    }
+}
+
+console.log(person1.name);
+console.log(person1.age);
+person1.greet()
+
+
+console.log(person2);
+
+
